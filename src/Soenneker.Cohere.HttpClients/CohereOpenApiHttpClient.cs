d8999b064cdcf0ea5c14ae6ea342d0bc3ef898e9
soenneker <jake@soenneker.com>
2026-09-03 22:02:06 -0500

    Add interface inheritdoc references

--- src/Soenneker.Cohere.HttpClients/CohereOpenApiHttpClient.cs
+++ src/Soenneker.Cohere.HttpClients/CohereOpenApiHttpClient.cs
@@ -11,6 +11,7 @@ using Soenneker.Utils.HttpClientCache.Abstract;
 
 namespace Soenneker.Cohere.HttpClients;
 
+/// <inheritdoc cref="ICohereOpenApiHttpClient" />
 public sealed class CohereOpenApiHttpClient : ICohereOpenApiHttpClient
 {
     private readonly IHttpClientCache _httpClientCache;
